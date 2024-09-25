de12537ac8a82ffd926ca024195987bafe986a5a
Sailokeshg <sailokeshreddyg@gmail.com>
2024-09-24 20:51:57 -0700

    feat:react and tailwind added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first React App</h1>
+      <p>Welcome to my first React App</p>
+    </div>
+  );
+}
+
+export default App;
